1470508b57924250f65b6222c95e1fbb1273c312
starrrj <silverstar1226@gmail.com>
2022-03-15 19:16:34 +0900

    new

---
+++ src/VariableEx3.java
@@ -0,0 +1,18 @@
+
+public class VariableEx3 {
+	
+	public static void main(String[] args) {
+		
+		// var : variable 변수
+		int var1 = 10;					// 10진수로 초기화
+		System.out.println(var1);
+		
+		int var2 = 010;					// 8진수로 초기화
+		System.out.println(var2);
+		
+		int var3 = 0x10;				// 16진수로 초기화 : 보안솔루션만들 때
+		System.out.println(var3);
+
+	} // main
+	
+} // class
