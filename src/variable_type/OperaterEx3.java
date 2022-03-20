639005c068d1ab4a364ae88cda5d754a28dafde3
starrrj <silverstar1226@gmail.com>
2022-03-20 13:30:31 +0900

    Merge branch 'master' of https://github.com/starrrj/PacticeJava.git

---
+++ src/variable_type/OperaterEx3.java
@@ -0,0 +1,49 @@
+package variable_type;
+
+public class OperaterEx3 {
+
+
+	public static void main(String[] args) {
+			
+		int x = 10;
+		int y = 10;
+		int z;
+		
+			//* 증감연산자가 단독으로만 사용되는 경우에는, 전위/후위의 의미가 없다!
+		x++;                   //   -> 11
+		++x;                   //	-> 12
+
+		System.out.println("1.x=" + x); //  ---> 12
+			
+		y--;                    //   -> 9                            
+		--y;                    //   -> 8
+
+		System.out.println("2.y=" + y);  //  ---> 9
+		System.out.println("3. x="+ x);  // ---> 12
+			
+		z = x ++;          	//       -> 13
+
+		System.out.println("4. z=" + z); // ---> 12
+		System.out.println("5. x=" + x); 	 // ---> 13
+
+		z = ++x;                  	// --> 14
+
+		System.out.println("6. z=" + x); // ---> 14
+		System.out.println("7. x=" + x); // ---> 14
+			
+		//	* 가독성이 좋아야 좋은 코드, 클린코드
+		//  x : 전위 , y : 후위 (먼저 본인의 값으로 참여하여하고 그 후에 연산)
+	  	// 	z = ++x + y++;      -> 15 + 8 = 24 
+		// 	욕바가지로 먹는 코드
+		
+		x = x + 1;               //  -> 15 
+		z = x + y;               //  -> 23 
+		y = y + 1;               //  -> 9
+
+		System.out.println("8. z=" + z);
+		System.out.println("9. x=" + x);
+		System.out.println("10. y=" + y);
+
+	} // main	
+} // end class
+
---
+++ src/variable_type/PassEx.java
@@ -0,0 +1,11 @@
+package variable_type;
+
+public class PassEx {
+
+	public static void main(String[] args) {
+		
+		;;   // 아직 실행 문장을 완성하지 못하거나, 준비가 안되었을 때 사용
+				
+	} // main
+
+} // class
