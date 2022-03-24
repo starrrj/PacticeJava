d201b9581d47f6dd8c6d75a33b5e32e81cbfe1b9
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-03-24 09:18:49 +0900

    Add files via upload
    new

---
+++ src/default_method_use/RemoteControlExample.java
@@ -0,0 +1,24 @@
+package default_method_use;
+
+
+public class RemoteControlExample {
+
+	public static void main (String[] args) {
+		RemoteControl rc = null;
+		
+		rc = new Television();		// 다형성 -1
+		rc.turnOn();				// 다형성 -2
+		rc.setMute(true);			// 다형성 -2
+	
+//		--
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.setMute(true);
+		
+		// 디폴트 메소드 호출
+		
+	} // main
+
+}
+
