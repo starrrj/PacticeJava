b38a5032b6ec9653f5807e3ee5e719c52b34ade0
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-03-29 23:39:27 +0900

    다형성

---
+++ src/polymorphism/p2/C.java
@@ -0,0 +1,16 @@
+package polymorphism.p2;
+
+import polymorphism.p1.A;
+
+public class C {
+
+	public void method() {
+		A a;				// A 는 가능
+//		a = new A();		// XX : A 클래스는 public으로 접근이 가능하나
+							// 		생성자는 protect로 다른패키지에서는 접근이 불가능하다.
+//		a.field = "java";	// 필드 사용 불가
+//		a.method();			// 메소드 사용 불가
+		//
+	} // method
+
+} // end class
