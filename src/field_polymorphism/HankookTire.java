18d2b08fdee99c44bcc2d779e4619d86982154af
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-03-29 23:39:58 +0900

    필드 다형성

---
+++ src/field_polymorphism/HankookTire.java
@@ -0,0 +1,11 @@
+package field_polymorphism;
+
+
+public class HankookTire implements Tire {
+
+	@Override
+	public void roll() {
+		System.out.println("HankookTire:: roll() invoked.");
+	} // roll
+	
+}// end class
---
+++ src/field_polymorphism/Tire.java
@@ -0,0 +1,8 @@
+package field_polymorphism;
+
+
+public interface Tire {		// 추상메서드
+	
+	public abstract void roll();
+	
+} // end interface
