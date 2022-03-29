9861cb4a694c33a1a299ab1a96f116056d7b1ed4
starrrj <silverstar1226@gmail.com>
2022-03-29 23:30:11 +0900

    Update

--- src/promation/C.java
+++
@@ -1,5 +0,0 @@
-package promation;
-
-public class C extends A{
-	;;
-}
--- src/user_define_exception/BalanceInsufficientException.java
+++
@@ -1,9 +0,0 @@
-package user_define_exception;
-
-public class BalanceInsufficientException extends Exception {
-	public BalanceInsufficientException() {}
-	public BalanceInsufficientException(String message) {
-		super(message);
-	}
-	
-}
