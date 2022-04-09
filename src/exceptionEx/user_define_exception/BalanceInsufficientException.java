de6ba0525815a59217f3f234882b524190e152f7
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-04-09 20:15:33 +0900

    Delete src/exceptionEx directory

--- src/exceptionEx/user_define_exception/BalanceInsufficientException.java
+++
@@ -1,9 +0,0 @@
-package exceptionEx.user_define_exception;
-
-public class BalanceInsufficientException extends Exception {
-	public BalanceInsufficientException() {}
-	public BalanceInsufficientException(String message) {
-		super(message);
-	}
-	
-}
