39da9c937c8a14578c8075e2fb87fda7f32e8659
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-06-19 23:43:16 +0900

    Add files via upload

--- src/baekjoon/java_10172.java
+++ src/baekjoon/java_10172.java
@@ -3,7 +3,7 @@ package baekjoon;
 public class java_10172 {
 
 	public static void main(String[] args) {
-		System.out.println("|\\_/|\n|q p|");
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
 
 	}
 
