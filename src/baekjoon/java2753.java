701ccbfd364ec47333cb954699151e7d53583b4a
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-07-18 22:56:52 +0900

    Add files via upload

---
+++ src/baekjoon/java2753.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class java2753 {
+
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		sc.close();
+		
+		if(a%4==0 && a%100!=0) System.out.println("1"); 
+		else if(a%400==0) System.out.println("1");
+		else System.out.println("0");
+	}
+}
