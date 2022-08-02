f679092a5741b9937f960a78c11a5f1d34a11246
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-08-02 22:19:08 +0900

    Add files via upload

---
+++ src/baekjoon/Java2884.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Java2884 {
+	
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+	
+		sc.close();
+		
+		if (m<45) {
+			h--;
+			m = 60 - (45 -m);
+			
+			if(h<0) {
+				h =23;
+			}
+				System.out.println(h + " " + m);
+			
+		} else {
+			System.out.println(h+ " " + (m-45));
+		}
+		
+	}
+}
